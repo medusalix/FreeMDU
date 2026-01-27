bdfaccf56dcf5def442f6e20fedf9b287297fd2c
Severin von Wnuck-Lipinski <severinvonw@outlook.de>
2026-01-27 14:26:09 +0100

    Update dump examples for 32-bit reads

--- protocol/examples/dump_memory.rs
+++ protocol/examples/dump_memory.rs
@@ -8,6 +8,10 @@ use std::{
 async fn main() -> Result<(), Box<dyn Error>> {
     env_logger::init();
 
+    // Adjust address range here
+    const START: u32 = 0x0000_0000;
+    const END: u32 = 0x0000_ffff;
+
     let mut port = freemdu::serial::open("/dev/ttyACM0")?;
     let mut dev = freemdu::device::connect(&mut port).await?;
     let mut file = OpenOptions::new()
@@ -16,10 +20,10 @@ async fn main() -> Result<(), Box<dyn Error>> {
         .open("memory_dump.bin")?;
 
     // Resume dumping process if previously interrupted
-    let start: u16 = file.seek(SeekFrom::End(0))?.try_into()?;
+    let offset: u32 = file.seek(SeekFrom::End(0))?.try_into()?;
 
-    for addr in (start..=0xffff).step_by(0x80) {
-        println!("Reading memory address {addr:04x}");
+    for addr in (START + offset..=END).step_by(0x80) {
+        println!("Reading memory address {addr:08x}");
 
         let data: [u8; 0x80] = dev.interface().read_memory(addr).await?;
 
