ed91f4ae5fb74741c0da1d780dad76a58c8f2826
Severin von Wnuck-Lipinski <severinvonw@outlook.de>
2026-02-06 17:15:02 +0100

    Update dump examples for 32-bit reads

--- protocol/examples/dump_eeprom.rs
+++ protocol/examples/dump_eeprom.rs
@@ -8,6 +8,10 @@ use std::{
 async fn main() -> Result<(), Box<dyn Error>> {
     env_logger::init();
 
+    // Adjust address range here
+    const START: u16 = 0x0000;
+    const END: u16 = 0x07ff;
+
     let mut port = freemdu::serial::open("/dev/ttyACM0")?;
     let mut dev = freemdu::device::connect(&mut port).await?;
     let mut file = OpenOptions::new()
@@ -16,9 +20,9 @@ async fn main() -> Result<(), Box<dyn Error>> {
         .open("eeprom_dump.bin")?;
 
     // Resume dumping process if previously interrupted
-    let start: u16 = file.seek(SeekFrom::End(0))?.try_into()?;
+    let offset: u16 = file.seek(SeekFrom::End(0))?.try_into()?;
 
-    for addr in (start..=0x07ff).step_by(0x80) {
+    for addr in (START + offset..=END).step_by(0x80) {
         println!("Reading EEPROM address {addr:04x}");
 
         let data: [u8; 0x80] = dev.interface().read_eeprom(addr / 2).await?;
