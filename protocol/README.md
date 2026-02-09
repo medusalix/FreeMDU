8fc867ea9da783ec898cf482e61b30c818f776a2
gopromaster <felixalbrecht@icloud.com>
2026-02-09 18:31:07 +0100

    move examples to src/bin, add command line arguments, and add clap as optional dependency

--- protocol/README.md
+++ protocol/README.md
@@ -14,16 +14,31 @@ When adding this crate as a dependency, the following optional features can be s
 
 - **`native-serial`**: enables a serial port implementation based on the [`serial2-tokio`](https://crates.io/crates/serial2-tokio) crate (requires `std`)
 
-## Examples
+## Binaries
 
-Several examples are provided to demonstrate the crate's functionality:
+Several binaries are provided to work with your devices:
 
 - **`find_keys`**: finds the diagnostic keys of a device using a brute-force search
 - **`dump_memory`**: reads RAM and ROM data from a supported device and writes them to a file
 - **`dump_eeprom`**: reads the EEPROM contents from a supported device and writes them to a file
 
-An example can be executed with the following command, replacing `<EXAMPLE>` with the desired example name:
+A binary can be executed with the following command, replacing `<BIN>` with the desired binary name:
 
 ```shell
-cargo run --all-features --example <EXAMPLE>
+cargo run --all-features --bin <BIN>
 ```
+
+All binaries accept the flag `-p` to define the serial port of your ESP32, the default is:
+
+```shell
+cargo run --all-features --bin <BIN> -- -p /dev/ttyACM0
+```
+
+For `dump_eeprom`, you can also use these flags (they are the default values):
+- specify start and/or end addresses: `-s 0x0000 -e 0x07ff`
+- specify output filename: `-o eeprom_dump.bin`
+- use byte instead of word addressing for newer devices: `-b`
+
+For `dump_memory`:
+- start/end 32-bit addresses: `-s 0x0000_0000 -e 0x0000_ffff`
+- output file: `-o memory_dump.bin`
--- protocol/src/bin/find_keys.rs
+++ protocol/src/bin/find_keys.rs
@@ -1,3 +1,4 @@
+use clap::Parser;
 use freemdu::{Interface, serial::Port};
 use std::{error::Error, thread, time::Duration};
 use tokio::time;
@@ -6,8 +7,24 @@ const UNLOCK_TIMEOUT: Duration = Duration::from_millis(500);
 const ERROR_RETRY_DELAY: Duration = Duration::from_secs(4);
 const CHECK_TIMEOUT: Duration = Duration::from_millis(100);
 
+// First try valid keys for existing models
+const READ_KEYS: [u16; 8] = [
+    0x43ea, 0xb4ee, 0x1234, 0x2b67, 0x04d2, 0x2e69, 0x15a8, 0xbde7,
+];
+const FULL_ACCESS_KEYS: [u16; 8] = [
+    0x1f02, 0x4e83, 0x5678, 0x8235, 0x162e, 0x3e3b, 0x703d, 0x902f,
+];
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// Serial port path
+    #[arg(short, long, default_value = "/dev/ttyACM0")]
+    port: String,
+}
+
 async fn find_read_access_key(intf: &mut Interface<Port>) -> Result<u16, Box<dyn Error>> {
-    for i in 0x0000..=0xffff {
+    for i in READ_KEYS.iter().copied().chain(0x0000u16..=0xffffu16) {
         println!("Trying read access key: {i:04x}");
 
         while let Err(err) = time::timeout(UNLOCK_TIMEOUT, async {
@@ -33,7 +50,11 @@ async fn find_full_access_key(
     intf: &mut Interface<Port>,
     read_key: u16,
 ) -> Result<u16, Box<dyn Error>> {
-    for i in 0x0000..=0xffff {
+    for i in FULL_ACCESS_KEYS
+        .iter()
+        .copied()
+        .chain(0x0000u16..=0xffffu16)
+    {
         println!("Trying read & full access keys: {read_key:04x}, {i:04x}");
 
         while let Err(err) = time::timeout(UNLOCK_TIMEOUT, async {
@@ -60,8 +81,14 @@ async fn find_full_access_key(
 async fn main() -> Result<(), Box<dyn Error>> {
     env_logger::init();
 
-    let port = freemdu::serial::open("/dev/ttyACM0")?;
+    let args = Args::parse();
+
+    let port = freemdu::serial::open(&args.port)?;
     let mut intf = Interface::new(port);
+    let id = intf.query_software_id().await?;
+
+    println!("Connected to device with Software ID: {id:04x}");
+
     let read_key = find_read_access_key(&mut intf).await?;
     let full_key = find_full_access_key(&mut intf, read_key).await?;
 
